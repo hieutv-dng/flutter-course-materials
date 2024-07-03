73520c3e96807c56eb1485ffc94a5da8aab44a08
Hieu Tran <hieutv-dng@github.com>
2024-07-03 22:15:30 +0700

    ! Update Flutflix Tutorial Project

--- flutflix_tutorial/lib/core/constants.dart
+++ flutflix_tutorial/lib/core/constants.dart
@@ -1,4 +1,4 @@
 class Constants {
-  static const apiKey = 'db0f427f98571220ebd228efc0361b51';
+  static const apiKey = '<TMDB_API_KEY>';
   static const imagePath = 'https://image.tmdb.org/t/p/w500';
 }
